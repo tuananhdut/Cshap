1d5234e76fbe8aa7c1863fef5d944579e4ed91bf
tuananh <tuanhaoggg@gmail.com>
2023-01-04 23:51:20 +0700

    swap

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -2,8 +2,20 @@
 
 namespace helloworld{
     internal class Program{
+        static void swap(ref int a, ref int b){
+            int temp=a; 
+            a=b; 
+            b=temp;
+        } 
         static void Main(){
-            Console.WriteLine("hello");
+            int a,b;
+            a=int.Parse(Console.ReadLine());
+            b=int.Parse(Console.ReadLine());
+            Console.WriteLine("a={0}",a);
+            Console.WriteLine("b={0}",b);
+            swap(ref a, ref b);
+            Console.WriteLine("a={0}",a);
+            Console.WriteLine("b={0}",b);
         }
     }
 }
