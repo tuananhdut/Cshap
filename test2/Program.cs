b8217f58f3fcb4067dcb228605f79981b5b33b82
tuananh <tuanhaoggg@gmail.com>
2023-01-05 11:39:25 +0700

    add test2

---
+++ test2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace test2{
+    internal class Program{
+        static void swap(ref int a, ref int b){
+            int temp=a; 
+            a=b; 
+            b=temp;
+        } 
+        static void Main(){
+            int a,b;
+            a=int.Parse(Console.ReadLine());
+            b=int.Parse(Console.ReadLine());
+            Console.WriteLine("a={0}",a);
+            Console.WriteLine("b={0}",b);
+            swap(ref a, ref b);
+            Console.WriteLine("a={0}",a);
+            Console.WriteLine("b={0}",b);
+            int c;
+            Console.ReadKey();
+        }
+    }
+}
